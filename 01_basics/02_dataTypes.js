db47e6cb70098f46b8b1c607ddcd8a26bdcabef5
Utkarshdubey44 <72154732+Utkarshdubey44@users.noreply.github.com>
2025-06-27 22:10:24 +0000

    datatypes in JS

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodeJS not browser hence it will give error
+
+console.log(3 
+    + 3)        //code readibility should be high
+
+console.log("Dubey")
+
+
+let name = "Dubey"
+let age = 24
+let isLoggedIn = false
+
+                    // Primitive DataTypes
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not defined
+// symbol => unique
+
+console.log(typeof undefined);   //undefined
+
+console.log(typeof null);   //object
