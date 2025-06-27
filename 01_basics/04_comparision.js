f66a1211092fd6dbcfaa1f56c31ecf01898aafef
Utkarshdubey44 <72154732+Utkarshdubey44@users.noreply.github.com>
2025-06-27 22:52:31 +0000

    comparisions edge cases

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 > 14);
+// console.log(2 > 2);
+// console.log(2 >= 2);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);    // true
+// console.log("02" > 1);   // true
+
+// console.log(null > 0);      // false
+// console.log(null == 0);     // false
+// console.log(null >= 0);     // true
+
+
+// console.log(undefined == 0);    // false
+// console.log(undefined > 0);     // false
+// console.log(undefined < 0);     // false
+
+// Avoid above conversions from line 8 and below as they can cause inconsistency and not give readability.
