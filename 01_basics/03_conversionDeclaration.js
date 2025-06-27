ab9eaaf4dfe9059128a39313966c327ac3ecd01a
Utkarshdubey44 <72154732+Utkarshdubey44@users.noreply.github.com>
2025-06-27 22:23:13 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionDeclaration.js
@@ -0,0 +1,31 @@
+let score = null
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33a" => NaN
+// true => 1, false => 0
+// undefined => NaN
+// null => 0
+
+            // Boolean Conversion
+let isLoggedIn = null
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// "Nik" => true
+// "" => false, null => false, 0 => false
+
+
+// To string conversion
+let someNumber = null
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
